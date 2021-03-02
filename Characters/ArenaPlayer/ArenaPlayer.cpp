bc03e8289882d81a95dce6f1756bfcbb24d355ad
Tanner <43326002+tboned@users.noreply.github.com>
2021-03-02 11:19:09 -0600

    added a shift walk, new default, crouch, and walk speeds

--- Characters/ArenaPlayer/ArenaPlayer.cpp
+++ Characters/ArenaPlayer/ArenaPlayer.cpp
@@ -1,20 +1,20 @@
 // Create Character Pawn
-	//getplayercontroller
-		//AutoPossessPlayer = EAutoReceiveInput::Player0;
-	//create movement
+	//getplayercontroller	X
+		//AutoPossessPlayer = EAutoReceiveInput::Player0;	X
+	//create movement	X
 		//non-rigid body physics
-		//character movement component
+		//character movement component 
 			//setup CharacterMovementComponent properties.
 	//create mesh
 		//skeletal mesh component
 		//skeletal mesh animation
 			//root motion params for CharacterMovementComponent.
-	//create collision
-		//capsule component
-			//trigger capsule
+	//create collision	
+		//capsule component		X
+			//trigger capsule	X
 			//collisions with static mesh
-	//create camera component	
-		//setup fov
+	//create camera component	X
+		//setup fov		
 		
 
 #include "ArenaPlayer.h"
@@ -22,7 +22,7 @@
 #include "Components/InputComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFrameWork/InputSettings.h"
+#include "GameFramework/InputSettings.h"
 #include "Components/CapsuleComponent.h"
 
 // Sets default values
@@ -30,14 +30,14 @@ AArenaPlayer::AArenaPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	
+
 	//setup camera
 	FPCameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	FPCameraComp->SetupAttachment(GetCapsuleComponent());
 	FPCameraComp->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f));	//cam position
 	FPCameraComp->bUsePawnControlRotation = true;	//for control
 
-	
+
 	//setup first person skeletal mesh
 	FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPS Mesh"));
 	FPSMesh->SetOnlyOwnerSee(true);
@@ -46,26 +46,36 @@ AArenaPlayer::AArenaPlayer()
 	FPSMesh->CastShadow = false;
 	FPSMesh->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
 	FPSMesh->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
-	
-	
+
+
 	//setup triggercapsule
 	TriggerCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Trigger Capsule"));
 	TriggerCapsule->InitCapsuleSize(55.f, 96.f);
 	TriggerCapsule->SetCollisionProfileName(TEXT("Trigger"));
 	TriggerCapsule->SetupAttachment(RootComponent);
 
+
 	//default possess.
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
+	//init movmt vals
 	BaseTurnRate = 45.0f;
 	BaseLookUpAtRate = 45.0f;
+
+	//init movmt vals
+	MaxCrouchspeed = 100.f;
+	WalkSpeed = 250.f;
+	DefaultSpeed = 500.f;
+
+	GetCharacterMovement()->MaxWalkSpeed = DefaultSpeed;
+	GetCharacterMovement()->MaxWalkSpeedCrouched = MaxCrouchspeed;
 }
 
 // Called when the game starts or when spawned
 void AArenaPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -84,6 +94,15 @@ void AArenaPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
+	//crouch
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AArenaPlayer::StartCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AArenaPlayer::StopCrouch);
+
+	//walk
+	PlayerInputComponent->BindAction("Walk", IE_Pressed, this, &AArenaPlayer::StartWalk);
+	PlayerInputComponent->BindAction("Walk", IE_Released, this, &AArenaPlayer::StopWalk);
+
+	//move
 	PlayerInputComponent->BindAxis("MoveForward", this, &AArenaPlayer::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AArenaPlayer::MoveRight);
 
@@ -122,6 +141,24 @@ void AArenaPlayer::MoveRight(float Value)
 	}
 }
 
+void AArenaPlayer::StartWalk()
+{
+	if (!bIsCrouched)
+	{
+		bIsWalking = true;
+		GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	}
+}
+
+void AArenaPlayer::StopWalk()
+{
+	if (!bIsCrouched)
+	{
+		bIsWalking = false;
+		GetCharacterMovement()->MaxWalkSpeed = DefaultSpeed;
+	}
+}
+
 //turnatrate def, how this character turns
 void AArenaPlayer::TurnAtRate(float Value)
 {
@@ -137,21 +174,17 @@ void AArenaPlayer::LookUpAtRate(float Value)
 //startcrouch def
 void AArenaPlayer::StartCrouch()
 {
-	if (!GetCharacterMovement()->IsCrouching())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Crouching"));
-		GetCharacterMovement()->bWantsToCrouch = true;
-		GetCharacterMovement()->Crouch();
-	}
+	GetCharacterMovement()->MaxWalkSpeedCrouched = MaxCrouchspeed;
+	Crouch();
 }
 
 //stop crouch def
 void AArenaPlayer::StopCrouch()
 {
-	if (GetCharacterMovement()->IsCrouching())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("UnCrouching"));
-		GetCharacterMovement()->bWantsToCrouch = false;
-		GetCharacterMovement()->UnCrouch();
-	}
+	UnCrouch();
+}
+
+bool AArenaPlayer::IsWalking()
+{
+	return bIsWalking;
 }
