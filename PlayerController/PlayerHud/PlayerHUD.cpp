b1745a6b8df7f6e7849cf34766308bad02f50222
Tanner <43326002+tboned@users.noreply.github.com>
2021-03-07 13:04:26 -0600

    defined the show and remove menu functions, to toggle the main menu

--- PlayerController/PlayerHud/PlayerHUD.cpp
+++ PlayerController/PlayerHud/PlayerHUD.cpp
@@ -2,8 +2,46 @@
 
 
 #include "PlayerHUD.h"
+#include "SMainMenu.h"
+#include "Widgets/SWeakWidget.h"
+#include "Engine/Engine.h"
+#include "GameFramework/PlayerController.h"
 
-APlayerHUD::APlayerHUD()
+void APlayerHUD::BeginPlay()
 {
+	Super::BeginPlay();
 
-};
+	ShowMenu();
+
+}
+
+void APlayerHUD::ShowMenu()
+{
+	if (GEngine && GEngine->GameViewport)
+	{
+		MenuWidget = SNew(SMainMenu).OwningHUD(this);
+		GEngine->GameViewport->AddViewportWidgetContent(SAssignNew(MenuWidgetContainer, SWeakWidget).PossiblyNullContent(MenuWidget.ToSharedRef()));
+
+		if (PlayerOwner)
+		{
+			PlayerOwner->bShowMouseCursor = true;
+			PlayerOwner->SetInputMode(FInputModeUIOnly());
+		}
+	}
+}
+
+void APlayerHUD::RemoveMenu()
+{
+	if (GEngine && GEngine->GameViewport && MenuWidgetContainer.IsValid())
+	{
+		GEngine->GameViewport->RemoveViewportWidgetContent(MenuWidgetContainer.ToSharedRef());
+
+		if (PlayerOwner)
+		{
+			PlayerOwner->bShowMouseCursor = false;
+			PlayerOwner->SetInputMode(FInputModeGameOnly());
+		}
+
+
+	}
+}
